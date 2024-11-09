28bbc367858d35d8b1612ff5eb5cfaecb2898bc0
Matthew Feickert <matthew.feickert@cern.ch>
2024-11-08 23:48:44 -0700

    feat: Add simple C++ library example that build with ATLAS CMake (#4)
    * Add C++ example based off of gitlab.com/CLIUtils/modern-cmake that builds
      with ATLAS CMake. 
      - c.f. https://gitlab.com/CLIUtils/modern-cmake/-/tree/861f0ce7052b0826261378e412e58533e1982ce4/examples/simple-project
      - ATLAS CMake uses 'atlas_subdir' instead of 'project' in CMakeLists.txt files
        and a package_filters.txt passed as a CMake argument.
    * Add build_atlas_cmake.sh build script.
    * Update README instructions to build from the top level of the repository.
    * Add build directories to .gitignore.

---
+++ build_atlas_cmake.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Setup release
+if [[ -f /release_setup.sh ]]; then
+    echo -e "\n# Assuming inside a Linux container"
+    echo -e "\n# . /release_setup.sh\n"
+    . /release_setup.sh
+else
+    echo -e "\n# setupATLAS\n"
+    export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+    # Allows for working with wrappers as well
+    . "${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh" --quiet || echo "~~~ERROR: setupATLAS failed!~~~"
+
+    echo -e "\n# lsetup git\n"
+    lsetup git
+
+    # Just need to be post Analysis release v25.2.30
+    echo -e "\n# asetup AnalysisBase,25.2.30\n"
+    asetup AnalysisBase,25.2.30
+fi
+
+if [[ -d build_atlas_cmake ]]; then
+    echo -e "\n# rm -rf build_atlas_cmake\n"
+    rm -rf build_atlas_cmake
+fi
+
+echo -e "\n# cmake -DATLAS_PACKAGE_FILTER_FILE=atlas_cmake/package_filters.txt -S atlas_cmake/Projects/WorkDir -B build_atlas_cmake\n"
+cmake \
+    -DATLAS_PACKAGE_FILTER_FILE=atlas_cmake/package_filters.txt \
+    -S atlas_cmake/Projects/WorkDir \
+    -B build_atlas_cmake
+
+echo -e "\n# cmake build_atlas_cmake -LH\n"
+cmake build_atlas_cmake -LH
+
+echo -e "\n# cmake --build build_atlas_cmake --clean-first --parallel 8\n"
+# cmake \
+#     --build build_atlas_cmake \
+#     --clean-first \
+#     --parallel $(nproc --ignore=1)
+cmake \
+    --build build_atlas_cmake \
+    --clean-first \
+    --parallel 8
+
+echo -e "\n# cd build_atlas_cmake\n"
+cd build_atlas_cmake
+
+echo -e "\n# Setup environment\n# . $(find . -type f -iname "setup.sh")\n"
+. $(find . -type f -iname "setup.sh")
+
+echo -e "\n# find . -type f -iname '*examplelib*'\n"
+find . -type f -iname '*examplelib*'
+
+echo -e "\n# example-bin\n"
+example-bin
